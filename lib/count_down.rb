6b5e897d29e1acec885359c4e2c61ec420df4df8
Mary Dick <maryclairedick@gmail.com>
2020-07-27 20:31:32 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,7 @@
 # Write your code here
-count = 0
-while count < 10 do
-  puts "I am the #{count}, I love to count!"
+count = 10
+while count >= 1 do
+  puts "#{count}"
   count += 1
 end
+puts "Happy New Year!"
